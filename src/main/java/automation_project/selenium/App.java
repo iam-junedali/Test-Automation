0c53f233da89f11efb2f38ed0af3f27de4174bd3
junedali1995 <junedali.sk@gmail.com>
2023-06-23 01:03:18 +0530

    commit

---
+++ src/main/java/automation_project/selenium/App.java
@@ -0,0 +1,13 @@
+package automation_project.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
